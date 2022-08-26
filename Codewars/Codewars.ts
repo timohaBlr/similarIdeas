10bc5571cb0f99c3d62377c25022740760a8cff8
timohaBlr <94timoha@gmail.com>
2022-08-26 09:09:22 +0300

    Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
    If you want to know more: http://en.wikipedia.org/wiki/DNA
    
    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
    
    More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
    
    Example: (input --> output)
    
    "ATTGC" --> "TAACG"
    "GTAT" --> "CATA"

--- Codewars/Codewars.ts
+++ Codewars/Codewars.ts
@@ -1,11 +1,5 @@
-function isIsogram(str){
-    let lowCase = str.toLowerCase();
-    for (let i = 0; i <= str.length; ++i) {
-        let newStr = lowCase.substring(i+1, str.length)
-        console.log(newStr);
-        if (newStr.indexOf(lowCase[i], 0) !==-1) {
-            console.log(lowCase[i]);
-            return false
-        }
-    } return true
+function DNAStrand(dna){
+    return dna.split('')
+        .map(item => item ==='A' ? item ='T' : item ==='T' ? item ='A' : item ==='C' ? item ='G' : item ==='G' ? item = 'C' : 'C')
+        .join('');
 }
